3a51743845a43e792ef1cd9d7d54760c7d6e7d67
Daniel Torres <danieltorres2902@gmail.com>
2024-02-03 08:47:15 -0300

    fixed font size problem

--- init.el
+++ init.el
@@ -24,3 +24,16 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-pyright dap-mode lsp-ivy lsp-ui lsp-mode flycheck magit org-bullets doom-modeline doom-themes dashboard projectile nerd-icons all-the-icons-ivy all-the-icons-dired all-the-icons counsel ivy company-box company which-key evil-collection evil goto-chg undo-tree)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
