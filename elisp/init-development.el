f43eb62f531f78798a0dd8cfd8ac817abc62779f
Daniel Torres <danieltorres2902@gmail.com>
2024-04-27 16:51:43 -0300

    updated development.el

--- elisp/init-development.el
+++ elisp/init-development.el
@@ -2,17 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (setq c-default-style "linux"
-;;       c-basic-offset 4)
-
-;; optionally if you want to use debugger
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-(use-package dap-mode
-  :ensure t)
-
-(use-package magit
-  :ensure t)
-
 (setq treesit-language-source-alist
       '((bash "https://github.com/tree-sitter/tree-sitter-bash")
         (cmake "https://github.com/uyha/tree-sitter-cmake")
@@ -32,49 +21,26 @@
 
 (setq major-mode-remap-alist
       '((javascript-mode . javascript-ts-mode)
-        (typescript-mode . typescript-ts-mode)
-        (python-mode . python-ts-mode)
-        (sh-mode . bash-ts-mode)))
+        ))
 
 
-(use-package python
-  :bind (:map python-ts-mode-map
-              ("<f5>" . recompile)
-              ("<f6>" . eglot-format))
-  :hook ((python-ts-mode . eglot-ensure)
-         (python-ts-mode . company-mode))
-  :mode (("\\.py\\'" . python-ts-mode)))
-
-(use-package highlight-indent-guides
-  :ensure t
-  :hook (python-ts-mode . highlight-indent-guides-mode)
-  :config
-  (set-face-foreground 'highlight-indent-guides-character-face "white")
-  (setq highlight-indent-guides-method 'character))
 
 (use-package javascript-ts-mode
   :hook ((javascript-ts-mode . eglot-ensure)
          (javascript-ts-mode . company-mode))
   :mode (("\\.js\\'" . javascript-ts-mode)))
 
-(use-package typescript-ts-mode
-  :hook ((typescript-ts-mode . eglot-ensure)
-         (typescript-ts-mode . company-mode))
-  :mode (("\\.ts\\'" . typescript-ts-mode)))
 
-(use-package tsx-ts-mode
-  :hook ((tsx-ts-mode . eglot-ensure)
-         (tsx-ts-mode . company-mode))
-  :mode (("\\.tsx\\'" . tsx-ts-mode)))
+;; (setq c-default-style "linux"
+;;       c-basic-offset 4)
 
-;; (use-package rjsx-mode
+;; optionally if you want to use debugger
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+;; (use-package dap-mode
 ;;   :ensure t)
 
-;; (use-package prettier-js
+;; (use-package magit
 ;;   :ensure t)
-;; (add-hook 'js2-mode-hook 'prettier-js-mode)
-;; (add-hook 'web-mode-hook 'prettier-js-mode)
-
 
 ;; (use-package flycheck
 ;;   :ensure t
