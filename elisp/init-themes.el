cd90bc4809bebe9edc37a666e5d56849596256b1
Daniel Torres <danieltorres2902@gmail.com>
2024-11-03 13:43:33 -0300

    updated init file

--- elisp/init-themes.el
+++ elisp/init-themes.el
@@ -62,22 +62,6 @@
 
 (global-set-key (kbd "M-d") 'dashboard-open)
 
-;;(setq custom-safe-themes t)
-;;(add-to-list 'custom-theme-load-path (concat user-emacs-directory "elisp/"))
-;;(add-hook 'after-init-hook (lambda () (load-theme 'xresources)))
-;;(add-hook 'emacs-startup-hook (lambda () (load-theme 'xresources)))
-
-;; (unless (package-installed-p 'spacemacs-theme)
-;;   (package-install 'spacemacs-theme))
-
-;; (use-package spacemacs-theme
-;;   :defer t
-;;   :init (load-theme 'spacemacs-dark t))
-
-;; (use-package doom-themes
-;;  :ensure t
-;;  :config
-;;  (load-theme 'doom-one t))
 
 (use-package doom-modeline
   :ensure t
